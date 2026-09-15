e62666dc70745154c3dc1affbf1e18eb40490baa
Belal Ahmad <belal.pgdca@gmail.com>
2026-09-16 01:07:32 +0530

    I have changed file names

---
+++ js1/basic1.js
@@ -0,0 +1,29 @@
+// this is Basic things of js
+
+// alert('Hello world');
+console.log("hello world");
+document.write("<h2>Hello Belal</h2>")
+
+// The first program of js
+// Statics Number
+let a = 5;
+let b = 4;
+var d = a + b ;
+console.log("The sum of "+a+" and "+b+" is : "+d);
+
+let ab = prompt('Put your value');
+let ba = prompt('Put your value');
+
+// var aa = ab+ba;
+console.log(ab + ba);
+
+
+const q=10;
+const w=15;
+
+const qw=q*w;
+console.log(q+w);
+
+
+
+
---
+++ js2/mathSqrt.js
@@ -0,0 +1,8 @@
+// here I practice square root of any number
+let a = prompt('Please enter a number','')
+let b = Math.sqrt (a);
+console.log(b);
+
+
+document.getElementsByTagName(h1).Mathematics;
+
